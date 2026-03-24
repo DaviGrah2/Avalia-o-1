1855839a29aac4e21d554339a5bda102d825b926
DaviGrah2 <davigrah2010@gmail.com>
2026-03-24 15:35:27 -0300

    Pls someone help me this

--- atv2/atv2.js
+++ atv2/atv2.js
@@ -1,21 +1,23 @@
 function frete_values() {
-    let peso = Number(prompt("Qual o peso do produto :"));
+    let peso;
     let valor_frete;
 
     if (peso <= 5) {
         valor_frete = 10.00
+        
     }
     if (peso > 5 && peso <= 20) {
         valor_frete = 20.00
+        
     }
     if (peso > 20) {
         valor_frete = 50.00
+        
     }
-    else {
-
+    if (peso < 0) {
         alert("O valor é invalido");
         frete_values()
-    }
+    };
 
-}
+};
 frete_values()
