613ebb14dd6ff0c9560e8e55afd60d2d056c2ddb
GustavoCamargo0 <gustavo.fscamargo08@gmail.com>
2026-03-24 16:07:08 -0300

    fixed atv2

--- atv2/atv2.js
+++ atv2/atv2.js
@@ -1,23 +1,29 @@
 function frete_values() {
-    let peso = 0.00;
+    let peso = parseFloat(document.getElementById("peso").value);
     let valor_frete = 0.00;
-
-    if (peso <= 5) {
+    if (isNaN(peso) || isNaN(valor_frete)) {
+        alert("Por favor, insira valores válidos para o frete.");
+        return;
+    }
+    else if (peso < 0 || valor_frete < 0) {
+        alert("Por favor, insira valores positivos para o frete.");
+        return;
+    }
+    else if (peso <= 5) {
         valor_frete = 10.00
-        
+
     }
-    if (peso > 5 && peso <= 20) {
+    else if (peso > 5 && peso <= 20) {
         valor_frete = 20.00
-        
+
     }
-    if (peso > 20) {
+    else if (peso > 20) {
         valor_frete = 50.00
-        
+
     }
-    if (peso < 0) {
+    else {
         alert("O valor é invalido");
-        frete_values() 
-    }
-    document.getElementById('valor_frete').innerText = "Valor do frete: R$ " + valor_frete.toFixed(2);
+        frete_values()
+    };
+    document.getElementById("valor_frete").innerText = "Valor do frete: R$ " + valor_frete.toFixed(2);
 };
-frete_values()
